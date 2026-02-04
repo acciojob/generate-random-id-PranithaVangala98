ead7fe201f20ac2e0ec886e429e5e29924c0cb1b
PranithaVangala98 <108581187+PranithaVangala98@users.noreply.github.com>
2026-02-04 09:57:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function makeid(l) {
   // write your code here
+	let alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234566789'
+	let randomletters = ''
+	for(let i=0;i<l;i++){
+		randomletters += alphabets.charAt(Math.floor(Math.random()*alphabets.length)) 
+	}
+	return randomletters
 }
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
+
 alert(makeid(l));
